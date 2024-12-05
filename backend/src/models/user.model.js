c83bd09bd9f3e6080188de1ba22e0d153627fc3d
varshapanda <varsha.vijayashwini@kalvium.community>
2024-12-05 16:21:58 +0530

    completed milestone4

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    Name:{ type : String, require : [ true, 'Please Enter the Name...'] },
+    email: {type:String, require:[true, 'Please Enter Email..'], 
+    unique:[true, 'Please enter Unique Email Address'],
+    },
+    password:{type:String, require:[true, 'Plesae enter the password..']},
+    address: [
+        {city:String},
+        {country:String},
+        {add1:String},
+        {add2:String},
+        {zipCode:String},
+        {addressType:String},
+    ],
+    role:{type:String, default:'user'},
+    avatar:{
+        url:{type:String, require:true},
+        public_id:{type:String, require:true},
+    },
+    resetPasswordToken: String,
+    resetPasswordTime: Date,
+},
+{
+    versionKey: false
+}
+
+);
+
+const userModel= mongoose.model('User', userSchema);
+module.exports = userModel;
