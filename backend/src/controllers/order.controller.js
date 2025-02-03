9ac6c99ba5eda40d7434dcadd403ab1adf845b3e
varshapanda <varsha.vijayashwini@kalvium.community>
2025-02-03 16:00:22 +0530

    milestome26-completed

--- backend/src/controllers/order.controller.js
+++ backend/src/controllers/order.controller.js
@@ -1,7 +1,8 @@
 const { default:mongoose} = require('mongoose');
 const OrderModel = require('../models/Order.model.js');
+const userModel = require('../models/user.model.js');
 
-async function CreateOrder(req, res){
+async function CreateOrderController(req, res){
     const userId = req.UserId;
     const {Items, address, totalAmount} = req.body; 
     try{
@@ -34,6 +35,31 @@ async function CreateOrder(req, res){
     }
 }
 
+async function GetUserOrdersController(req, res) {
+    const userId = req.UserId;
+    try {
+      if (!mongoose.Types.ObjectId.isValid(userId)) {
+        return res
+          .status(400)
+          .send({ message: 'In valid user id', success: false });
+      }
+      const checkUser = await userModel.findOne({ _id: userId });
+      if (!checkUser) {
+        return res
+          .status(400)
+          .send({ message: 'Please sign up', success: false });
+      }
+  
+      const orders = await OrderModel.find({ user: userId });
+      return res
+        .status(200)
+        .send({ message: 'Data Successfully fetched', success: true, orders });
+    } catch (er) {
+      return res.status(500).send({ message: er.message, success: false });
+    }
+}
+
 module.exports = {
-    CreateOrder,
+    CreateOrderController, 
+    GetUserOrdersController,
 }
--- backend/src/routes/order.route.js
+++ backend/src/routes/order.route.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const {verifyToken} = require('../middlewares/jwt-verify.js')
+const verifyUser = require('../middlewares/jwt-verify.js');
 
-router.post('/confirm-order', verifyToken)
+const {
+    GetUserOrdersController, 
+    CreateOrderController
+} = require('../controllers/order.controller.js');
+
+router.get('/user-orders-data', verifyUser, GetUserOrdersController);
+router.post('/confirm-order', verifyUser, CreateOrderController);
 
 module.exports = router;
